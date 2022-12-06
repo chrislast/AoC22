0603f5575f31e32a6cf35762b7d22f955a831dfb
Chris Last <chris.last@gmail.com>
2022-12-06 10:21:01 +0000

    day 6

---
+++ day6.py
@@ -0,0 +1,36 @@
+# import our helpers
+import sys
+from types import SimpleNamespace
+from utils import load, show, day, TRACE, Map, Path
+import visualizations as viz
+
+####### GLOBALS #########
+
+# load todays input data as a docstring
+TXT = load(day(__file__))
+# convenient for passing working between parts 1 and 2, and relevant stuff to vizualations 
+NS = SimpleNamespace(p1=[],p2=[])
+
+if len(sys.argv) > 1:
+    # Use test data instead
+    TXT = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+
+
+######## Part 1 ##########
+def p1(expect=1140):
+    sz=4
+    for i in range(len(TXT)):
+        if len(set(TXT[i:i+sz])) == sz:
+            return i+sz
+
+######## Part 2 ##########
+def p2(expect=3495):
+    sz=14
+    for _ in range(len(TXT)):
+        if len(set(TXT[i:i+sz])) == sz:
+            return i+sz
+
+if __name__ == "__main__":
+    show(p1, p2)
+    #viz.viz?p1(NS)
+    #viz.viz?p2(NS)
