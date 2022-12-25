313317861ab3526ffe9e2542a654381e8d91d98f
Chris Last <chris.last@gmail.com>
2022-12-25 10:22:39 +0000

    22

--- day22.py
+++ day22.py
@@ -100,15 +100,15 @@ def p1(expect=0 if USING_EXAMPLE else 0):
 class CubeMap:
     def __init__(self):
         # allow a border around map for edge reached
-        self.dim = 4 if USING_EXAMPLE else 50
+        self.dim3d = 6 if USING_EXAMPLE else 52
         self.rotations = []
-        self.load_from_2d_map()
-        self.pos = (1,1) # all the action happens on z=0 plane
+        self.from_2d()
+        self.x = self.y = 1 # all the action happens on z=0 plane
         self.set(ORIGIN)
         self.rotations = []
 
     def rotate_forward(self):
-        self.rotations.append("f")
+        self.rotations.append("rf")
         self.cube = np.rot90(self.cube,k=1,axes=(0,1))
 
     def rotate_backward(self):
@@ -125,20 +125,34 @@ class CubeMap:
 
     def turn_left(self):
         self.rotations.append("tl")
-        self.cube = np.rot90(self.cube,k=1,axes=(1,2))
-        self.pos = (self.dim-self.pos[1], self.pos[0])
+        saved = self.get()
+        self.set(DESTINATION)
+        self.cube = np.rot90(self.cube,k=1,axes=(2,1))
+        x,y = np.where(self.cube[0]==DESTINATION)
+        self.y, self.x = x[0], y[0]
+        self.set(saved)
 
     def turn_right(self):
         self.rotations.append("tr")
+        saved = self.get()
+        self.set(DESTINATION)
         self.cube = np.rot90(self.cube,k=1,axes=(1,2))
-        self.pos = (self.pos[1], self.dim-self.pos[0])
+        x,y = np.where(self.cube[0]==DESTINATION)
+        self.y, self.x = x[0], y[0]
+        self.set(saved)
 
     def set(self, val):
-        x,y = self.pos
-        self.cube[0,y,x] = val
+        self.cube[0,self.y,self.x] = val
 
-    def unload_to_2d_map(self):
-        self.set(DESTINATION)
+    def get(self):
+        return self.cube[0,self.y,self.x]
+
+    def to_2d(self):
+        # save cube state
+        cube = self.cube.copy()
+        x, y = self.x, self.y
+        rotations = self.rotations.copy()
+
         for inst in self.rotations[::-1]:
             match inst:
                 case "rr": self.rotate_left()
@@ -147,40 +161,45 @@ class CubeMap:
                 case "rb": self.rotate_forward()
                 case "tr": self.turn_left()
                 case "tl": self.turn_right()
+                case x: raise ValueError(x)
 
+        face2d = lambda: self.cube[0,1:-1,1:-1]
         if USING_EXAMPLE:
             map2d = np.zeros((12,16),dtype="uint8")
-            mapregion = slice(0,1:-1,1:-1)
-            map2d[0:4, 8:12] = self.cube[mapregion]
+            map2d[0:4, 8:12] = face2d()
             self.rotate_forward()
-            map2d[4:8, 8:12] = self.cube[0,1:-1,1:-1]
+            map2d[4:8, 8:12] = face2d()
             self.rotate_right()
-            map2d[4:8, 4:8 ] = self.cube[0,1:-1,1:-1]
+            map2d[4:8, 4:8 ] = face2d()
             self.rotate_right()
-            map2d[4:8, 0:4 ] = self.cube[0,1:-1,1:-1]
+            map2d[4:8, 0:4 ] = face2d()
             self.rotate_left()
             self.rotate_left()
             self.rotate_forward()
-            map2d[8:12, 8:12] = self.cube[0,1:-1,1:-1]
+            map2d[8:12, 8:12] = face2d()
             self.rotate_left()
-            map2d[8:12, 12:16] = self.cube[0,1:-1,1:-1]
+            map2d[8:12, 12:16] = face2d()
         else:
             map2d = np.zeros((200,150),dtype="uint8")
-            map2d[0:50,50:100] = self.cube[0,1:-1,1:-1]
+            map2d[0:50,50:100] = face2d()
             self.rotate_left()
-            map2d[0:50,100:150] = self.cube[0,1:-1,1:-1]
+            map2d[0:50,100:150] = face2d()
             self.rotate_right()
             self.rotate_forward()
-            map2d[50:100,50:100] = self.cube[0,1:-1,1:-1]
+            map2d[50:100,50:100] = face2d()
             self.rotate_forward()
-            map2d[100:150,50:100] = self.cube[0,1:-1,1:-1]
+            map2d[100:150,50:100] = face2d()
             self.rotate_right()
-            map2d[100:150,0:50] = self.cube[0,1:-1,1:-1]
+            map2d[100:150,0:50] = face2d()
             self.rotate_forward()
-            map2d[150:200,0:50] = self.cube[0,1:-1,1:-1]
+            map2d[150:200,0:50] = face2d()
+
+        self.cube = cube # restore original state
+        self.rotations = rotations
+        self.x, self.y = x, y
         return map2d
 
-    def load_from_2d_map(self):
+    def from_2d(self):
         if USING_EXAMPLE:
             self.cube = np.zeros((6,6,6),dtype="uint8")
             self.cube[0,1:-1,1:-1] = ARRAY[0:4, 8:12]
@@ -220,17 +239,28 @@ class CubeMap:
 
     def go_forward(self, steps):
         for _ in range(steps):
-            x,y = self.pos
-            if self.cube[0,y,x] == WALL:
-                # no further moves don't update pos
-                break
-            # go up
+            y = self.y
+            # go forward
             y -= 1
+
+            if self.cube[0,y,self.x] == WALL:
+                y += 1
+                break
+
             # if we are on rotate edge
             if y == 0:
-                self.rotate_forward()
-                # step back onto visible map
-                y = self.dim - 2
+                # rotate
+                self.rotate_backward()
+                # and step onto new cube face
+                y = 4 if USING_EXAMPLE else 50
+                # if we arrived at a wall
+                if self.cube[0, y, self.x] == WALL:
+                    # go back and stop
+                    self.rotate_forward()
+                    y = 1
+                    break
+
+            self.y = y
             self.set(PATH)
 
 
@@ -238,22 +268,28 @@ def p2(expect=0 if USING_EXAMPLE else 0):
     cube = CubeMap()
     instructions = D
     idx = 0
+    dbg = lambda: Map(cube.to_2d()).show()
+    cube.turn_right()
     while idx<len(instructions):
         if instructions[idx] == "L":
-            cube.rotate_left()
+            cube.turn_left() # move the world not santa!
             idx += 1
         elif instructions[idx] == "R":
-            cube.rotate_right()
+            cube.turn_right() # move the world not santa!
             idx += 1
         else:
             i = re.match(r"(?P<dist>[0-9]*).*", instructions[idx:])
             steps = i.groupdict()["dist"]
             cube.go_forward(int(steps))
             idx += len(steps)
-    breakpoint()
-    map2d = cube.unload_to_2d_map()
+    cube.set(DESTINATION)
+    map2d = cube.to_2d()
+    res = np.where(map2d==DESTINATION)
+    y,x = res[0]+1, res[1]+1
     m=Map(map2d)
     m.show()
+    breakpoint()
+    return y*1000+x*4
 
 
 if __name__ == "__main__":
